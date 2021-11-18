b2ee48114d58ea4686e84bf511616fb4606c36f1
DESKTOP-0V9VAH6\ashok <ashok.chakravarthi@zohocorp.com>
2021-11-18 12:43:24 +0530

    Second difference Test case passed.

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -20,6 +20,8 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = difference(15, 9, 1, 2, 3);
+  if (result !== 6) throw new Error('Expected difference(15, 9, 1, 2, 3) to be 6. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
